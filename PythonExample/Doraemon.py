869b14044130eb7ebec444440f1c537d923030af
zuolinye <zuolinye@163.com>
2018-06-20 15:48:44 +0800

    update example
    update Doraemon

--- PythonExample/Doraemon.py
+++ PythonExample/Doraemon.py
@@ -1,37 +1,180 @@
-# 导包
-import turtle as t
-# 定义画笔宽度
-t.pensize(5)
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-# 脸
-# 绘制图形的填充颜色为"blue"
-t.fillcolor("blue")
-# 画半径为200的圆
-t.circle(200)
-# 绘制图形的填充颜色为"白色"
-t.fillcolor("white")
-# 画半径为160的圆
-t.circle(160)
+from turtle import *
 
-# 鼻子
-# 将画笔移动到坐标为(0,200)的位置
-t.goto(0,200)
-# 画半径为20的圆
-t.circle(20)
+
+# 跳跃
+def my_goto(x, y):
+    penup()
+    goto(x, y)
+    pendown()
 
 # 眼睛
-t.goto(-45,250)
-t.circle(45)
-t.goto(-20,250)
-t.fillcolor("black")
-t.circle(15)
-t.goto(45,250)
-t.goto(20,250)
+def eyes():
+    tracer(False)
+    a = 2.5
+    for i in range(120):
+        if 0 <= i < 30 or 60 <= i < 90:
+            a -= 0.05
+            lt(3)
+            fd(a)
+        else:
+            a += 0.05
+            lt(3)
+            fd(a)
+    tracer(True)
 
-#smile
-t.goto(0,50)
-t.circle(150,extent = -75)
-print(t.pos())
-t.circle(150,extent = 75)
-t.left(15)
-t.forward(150)
+# 胡须
+def beard():
+    my_goto(-37, 135)
+    seth(165)
+    fd(60)
+
+    my_goto(-37, 125)
+    seth(180)
+    fd(60)
+
+    my_goto(-37, 115)
+    seth(193)
+    fd(60)
+
+    my_goto(37, 135)
+    seth(15)
+    fd(60)
+
+    my_goto(37, 125)
+    seth(0)
+    fd(60)
+
+    my_goto(37, 115)
+    seth(-13)
+    fd(60)
+
+# 嘴巴
+def mouth():
+    my_goto(5, 148)
+    seth(270)
+    fd(100)
+    seth(0)
+    circle(120, 50)
+    seth(230)
+    circle(-120, 100)
+
+# 围巾
+def scarf():
+    fillcolor('#e70010')
+    begin_fill()
+    seth(0)
+    fd(200)
+    circle(-5, 90)
+    fd(10)
+    circle(-5, 90)
+    fd(207)
+    circle(-5, 90)
+    fd(10)
+    circle(-5, 90)
+    end_fill()
+
+# 鼻子
+def nose():
+    my_goto(-10, 158)
+    fillcolor('#e70010')
+    begin_fill()
+    circle(20)
+    end_fill()
+
+# 黑眼睛
+def black_eyes():
+    seth(0)
+    my_goto(-20, 195)
+    fillcolor('#000000')
+    begin_fill()
+    circle(13)
+    end_fill()
+
+    pensize(6)
+    my_goto(20, 205)
+    seth(75)
+    circle(-10, 150)
+    pensize(3)
+
+    my_goto(-17, 200)
+    seth(0)
+    fillcolor('#ffffff')
+    begin_fill()
+    circle(5)
+    end_fill()
+    my_goto(0, 0)
+
+
+
+# 脸
+def face():
+    fd(183)
+    fillcolor('#ffffff')
+    begin_fill()
+    lt(45)
+    circle(120, 100)
+
+    seth(90)
+    eyes()
+    seth(180)
+    penup()
+    fd(60)
+    pendown()
+    seth(90)
+    eyes()
+    penup()
+    seth(180)
+    fd(64)
+    pendown()
+    seth(215)
+    circle(120, 100)
+    end_fill()
+
+# 头型
+def head():
+    penup()
+    circle(150, 40)
+    pendown()
+    fillcolor('#00a0de')
+    begin_fill()
+    circle(150, 280)
+    end_fill()
+
+# 画哆啦A梦
+def Doraemon():
+    # 头部
+    head()
+
+    # 围脖
+    scarf()
+
+    # 脸
+    face()
+
+    # 红鼻子
+    nose()
+
+    # 嘴巴
+    mouth()
+
+    # 胡须
+    beard()
+
+
+
+
+  
+    # 画眼睛
+    black_eyes()
+
+if __name__ == '__main__':
+    screensize(500,600, "#f0f0f0")
+    pensize(3)  # 画笔宽度
+    speed(9)    # 画笔速度
+    Doraemon()
+    my_goto(100, -100)
+    write('by zuolin', font=("Bradley Hand ITC", 30, "bold"))
+    mainloop()
